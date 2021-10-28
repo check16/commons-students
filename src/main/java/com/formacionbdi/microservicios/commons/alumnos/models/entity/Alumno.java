21ad742dbcb1dd8f08a6aa09b8cb8669506512ea
Antonio Sanchez <antonio.san.87@hotmail.com>
2021-10-28 19:15:56 +0200

    Added validation and pagination

--- src/main/java/com/formacionbdi/microservicios/commons/alumnos/models/entity/Alumno.java
+++ src/main/java/com/formacionbdi/microservicios/commons/alumnos/models/entity/Alumno.java
@@ -1,6 +1,8 @@
 package com.formacionbdi.microservicios.commons.alumnos.models.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Entity
@@ -11,8 +13,12 @@ public class Alumno {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
+    @NotEmpty
+    @Email
     private String email;
 
     @Column(name = "create_at")
